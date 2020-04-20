3307b79e47d686a05ad1af5386214173fa78eb7d
torco34 <torcoromaarias@gimail.com>
2020-04-20 00:49:36 -0500

    codigo JS 19/04/2020

---
+++ ArchJS/index.js
@@ -0,0 +1,8 @@
+let iconAlignJustify = document.querySelector('.icon-align-justify');
+let menu = document.querySelector('.lista-container');
+let containerMenu = document.querySelector('.menu');
+let activador =  true;
+
+iconAlignJustify.addEventListener('click', () => {
+	console.log('funciona')
+})
